4a50591172d7eca5dc1968de6de50d181ab4df24
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2019-01-27 19:59:40 +1100

    Update 1.js

--- scripts/1.js
+++ scripts/1.js
@@ -1,4 +1,8 @@
+// https://www.dforge.net/2014/03/12/javascript-and-regex-removing-trailing-pound-and-slash-from-the-url/
+// https://stackoverflow.com/a/12446376
 window.history.pushState('','',window.location.href.replace(/\/$/,''));
+
+// https://stackoverflow.com/a/260876
 (function() {
     var logo = document.createElement('link');
     logo.rel = 'icon';
