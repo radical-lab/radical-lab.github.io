24b8092adfe36155062ad54eefcbc9b84f4e6ddf
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2019-01-27 19:33:37 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 window.history.pushState('','',window.location.href.replace(/\/$/,''));
+(function() {
+    var logo = document.createElement('link');
+    logo.rel = 'icon';
+    logo.type = 'image/svg+xml';
+    logo.href = '/favicon.svg';
+    document.getElementsByTagName('head')[0].appendChild(logo);
+})();
