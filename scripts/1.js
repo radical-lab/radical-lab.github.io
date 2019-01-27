10538020c667d5c2fa4d7f14ca072582d5a0c27e
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2019-01-27 20:00:44 +1100

    Update 1.js

--- scripts/1.js
+++ scripts/1.js
@@ -1,11 +1,11 @@
 // https://www.dforge.net/2014/03/12/javascript-and-regex-removing-trailing-pound-and-slash-from-the-url/
 // https://stackoverflow.com/a/12446376
-window.history.pushState('','',window.location.href.replace(/\/$/,''));
+window.history.pushState('', '', window.location.href.replace(/\/$/, ''));
 
 // https://stackoverflow.com/a/260876
 (function() {
-    var logo = document.createElement('link');
-    logo.rel = 'icon';
+    var logo  = document.createElement('link');
+    logo.rel  = 'icon';
     logo.type = 'image/svg+xml';
     logo.href = '/favicon.svg';
     document.getElementsByTagName('head')[0].appendChild(logo);
