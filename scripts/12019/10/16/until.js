afddd0305e09e6258a0c5a4627127517f34f9160
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2019-10-16 09:22:47 +1100

    Update until.js

--- scripts/12019/10/16/until.js
+++ scripts/12019/10/16/until.js
@@ -1,3 +1,4 @@
+document.body.style.cursor = 'none';
 // the dereference of this pointer is inline.css.js
 alert("fullscreen the window yourself and go complain to the world that windows should be able to fullscreen themselves");
 // now it is t time
