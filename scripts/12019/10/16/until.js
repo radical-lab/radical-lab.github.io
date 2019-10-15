b12021ff0fe91071ba6c61736e0a6bc2209e27ae
Shyam-Has-Your-Anomaly-Mitigated <shyam@shyam.id.au>
2019-10-16 09:07:46 +1100

    meditative lacunæ

---
+++ scripts/12019/10/16/until.js
@@ -0,0 +1,11 @@
+// the dereference of this pointer is inline.css.js
+alert("fullscreen the window yourself and go complain to the world that windows should be able to fullscreen themselves");
+// now it is t time
+var t = new Date();
+// rescheduling t time to <span> the rest of the /day|night/ until some midpoint; excluding /mid((mou?rn|odd)|(even|bir?th?))ing/
+t.setHours(t.getHours() < 12? 12: 24);
+t.setMinutes(0);
+t.setSeconds(0);
+t.setMilliseconds(0);
+// do this at t time
+setTimeout(()=>alert("while it is true the display is currently blank this emptyness has a poetic meaning therefore it cannot be considered nothing as such and you are encouraged to fully acknowledge the emptyness of the experience but are you certain you do not wish to talk about your mother as i am a mock rogerian psychotherapist \\escvxﬄM thmbz arfgﬁ odwkﬂY ilyqﬀ nupjﬃW"), t.getTime() - Date.now());
